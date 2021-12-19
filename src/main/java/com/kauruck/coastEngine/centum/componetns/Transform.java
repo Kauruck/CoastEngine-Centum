b014d59434ffe1730b721dbec72ef29b6e612fb3
Adrian Kumbrink <adrian.kumbrink@gmx.de>
2021-12-19 16:34:19 +0100

    Transform component now has actual data.

--- src/main/java/com/kauruck/coastEngine/centum/componetns/Transform.java
+++ src/main/java/com/kauruck/coastEngine/centum/componetns/Transform.java
@@ -1,6 +1,36 @@
 package com.kauruck.coastEngine.centum.componetns;
 
 import com.kauruck.coastEngine.centum.component.AbstractComponent;
+import com.kauruck.coastEngine.core.math.Vector3;
 
 public class Transform extends AbstractComponent {
+
+    private Vector3 position;
+    private Vector3 rotation;
+    private float scale;
+
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3 position) {
+        this.position = position;
+    }
+
+    public Vector3 getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(Vector3 rotation) {
+        this.rotation = rotation;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
 }
